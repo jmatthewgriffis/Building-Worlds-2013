0a49921767b26ce8292a782900795f799c653635
J. Matthew Griffis <grifj153@newschool.edu>
2013-02-09 18:08:57 -0500

    Timing for emperor-trooper dialogue complete.

--- Week2hw(movie_w_scripting)/Assets/emperorMove.cs
+++ Week2hw(movie_w_scripting)/Assets/emperorMove.cs
@@ -1,17 +1,82 @@
 using UnityEngine;
 using System.Collections;
 
-public class emperorMove : MonoBehaviour {
+public class emperorMove : MonoBehaviour
+{
 	
 	public float speed = 7f;
+	public int timer = 0; // This controls when the movement boolean switches on.
+	public bool move = false; // This controls when the emperor moves.
+	public int moveState;
+	public float wait = 3; // This is how long the emperor waits before moving.
 	
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		moveState = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position += new Vector3 (0f, Mathf.Sin (Time.time * speed), 0f) * Time.deltaTime;
+	void Update ()
+	{
+		// Cue initial movement.
+		if (moveState == 0) {
+			if (timer < wait / Time.deltaTime) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 1;
+			}
+		}
+		// Limit initial movement.
+		else if (moveState == 1) {
+			if (timer < (wait / Time.deltaTime)) {
+				timer++;
+			} else {
+				move = false;
+				timer = 0;
+				moveState = 2;
+			}
+		}
+		else if (moveState == 2) {
+			if (timer < (wait / Time.deltaTime)*2.25) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 3;
+			}
+		}
+		else if (moveState == 3) {
+			if (timer < (wait / Time.deltaTime)*0.3) {
+				timer++;
+			} else {
+				move = false;
+				timer = 0;
+				moveState = 4;
+			}
+		}
+		else if (moveState == 4) {
+			if (timer < (wait / Time.deltaTime)) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 5;
+			}
+		}
+		
+		
+		//Debug.Log (timer);
+		
+		if (move == true) {
+			if (moveState < 5) {
+			transform.position += new Vector3 (0f, Mathf.Sin (Time.time * speed), 0f) * Time.deltaTime;
+			}
+			else {
+				transform.position += new Vector3 (0f, 0.25f, 0f) * Time.deltaTime;
+			}
+		}
 	}
 }
--- Week2hw(movie_w_scripting)/Assets/troopersMove.cs
+++ Week2hw(movie_w_scripting)/Assets/troopersMove.cs
@@ -11,16 +11,56 @@ public class troopersMove : MonoBehaviour
 	public float speed = 7f; // This limits the height of the trooper's bounce (the higher the number, the lower they bounce).
 	public int timer = 0; // This controls when the movement boolean switches on.
 	public bool move = false; // This controls when the troopers move.
-	public float wait = 15; // This is how long the troopers wait before moving.
+	public float wait = 8; // This is how long the troopers wait before moving.
+	public int moveState = 0;
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (timer < wait / Time.deltaTime) {
-			timer++;
+		if (moveState == 0) {
+			if (timer < wait / Time.deltaTime) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 1;
+			}
 		}
-		else {
-			move = true;
+		else if (moveState == 1) {
+			if (timer < (wait / Time.deltaTime)/3) {
+				timer++;
+			} else {
+				move = false;
+				timer = 0;
+				moveState = 2;
+			}
+		}
+		else if (moveState == 2) {
+			if (timer < (wait / Time.deltaTime)/2) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 3;
+			}
+		}
+		else if (moveState == 3) {
+			if (timer < (wait / Time.deltaTime)/8) {
+				timer++;
+			} else {
+				move = false;
+				timer = 0;
+				moveState = 4;
+			}
+		}
+		else if (moveState == 4) {
+			if (timer < (wait / Time.deltaTime)) {
+				timer++;
+			} else {
+				move = true;
+				timer = 0;
+				moveState = 5;
+			}
 		}
 		
 		//Debug.Log (timer);
@@ -30,8 +70,10 @@ public class troopersMove : MonoBehaviour
 			// Time.deltaTime is the interval between frames. This way the movement is proportionally consistent regardless of frame rate.
 			// Now use math to make the cubes bounce. Adjust the y-pos based on the sin of time passed.
 			transform.position += new Vector3 (0f, Mathf.Sin (Time.time * speed), 0f) * Time.deltaTime;
-			// What if you want to move a cube whatever direction is currently forward for it (presumably there's rotation)? Use this specialized Vector3:
-			transform.localPosition += transform.forward * Time.deltaTime;
+			if (moveState == 5) {
+				// What if you want to move a cube whatever direction is currently forward for it (presumably there's rotation)? Use this specialized Vector3:
+				transform.localPosition += transform.forward * Time.deltaTime;
+			}
 		}
 	}
 }
