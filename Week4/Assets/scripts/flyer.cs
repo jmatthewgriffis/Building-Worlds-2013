f96c1d725de4f8d7af50c30b520a9c80a967a2aa
J. Matthew Griffis <grifj153@newschool.edu>
2013-02-25 13:42:37 -0500

    Scripting with vector math.

--- Week4/Assets/scripts/flyer.cs
+++ Week4/Assets/scripts/flyer.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 using System.Collections;
 
 public class flyer : MonoBehaviour {
-
+	
+	Vector3 target;
+	float targetRange = 10f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		SetNewTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.position += ;
+	}
 	
+	void SetNewTarget() {
+		target = new Vector3 ( Random.Range(-targetRange, targetRange), Random.Range(-targetRange, targetRange), Random.Range(-targetRange, targetRange) );
 	}
 }
