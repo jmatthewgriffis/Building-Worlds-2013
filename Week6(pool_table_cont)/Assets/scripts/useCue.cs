0f953065f4d7e519973b853d1750beeb0088c3f2
J. Matthew Griffis <grifj153@newschool.edu>
2013-03-11 13:14:31 -0400

    Continuing work on the pool table in-class example.

---
+++ Week6(pool_table_cont)/Assets/scripts/useCue.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class useCue : MonoBehaviour {
+	
+	//public GameObject ball;
+	//public Vector3 moveBall = new Vector3(10,0,0);
+	
+	Vector3 cuePos;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//cuePos = new Vector3(
+			//Input(mousePos);
+		
+		//transform.position = Input.mousePosition;
+		
+		if (Input.GetMouseButton(0)) {
+			Debug.Log("Pressed!");
+			//ball.transform.position = transform.forward;
+		}
+	}
+}
