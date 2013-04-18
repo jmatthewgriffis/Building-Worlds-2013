7531c937d9bd8d7b4409805c3bb7b79306b826b1
J. Matthew Griffis <grifj153@newschool.edu>
2013-04-17 23:45:22 -0400

    Collision detection in both directions.

--- Week11(character_controller_with_raycasts)/Assets/scripts/moveCharacter.cs
+++ Week11(character_controller_with_raycasts)/Assets/scripts/moveCharacter.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class moveCharacter : MonoBehaviour {
 	
-	float moveSpeed;
-	float turnRate;
+	public float moveSpeed;
+	public float turnRate;
 
 	// Use this for initialization
 	void Start () {
 		
 		moveSpeed = 3.0f;
-		turnRate = 50.0f;
+		turnRate = 125.0f;
 	
 	}
 	
@@ -18,21 +18,26 @@ public class moveCharacter : MonoBehaviour {
 	void Update () {
 		
 		#region GRAVITY
-        if (!Physics.Raycast(transform.position, -transform.up, 1f)) {
-            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (!Physics.Raycast(transform.position, -transform.up, 1f)) { // Shoot a raycast downward; if it doesn't hit anything within 1 distance...
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime; // ...move downward ala gravity.
     	}
 		#endregion
 		
 		#region MOVING // This lets you collapse certain code sections easily.
-		bool capsuleCastHitWall = Physics.CapsuleCast(transform.position + transform.up,
-			transform.position - transform.up,
-			0.5f,
-			transform.forward,
-			1f);
+		bool capsuleCastHitWall = Physics.CapsuleCast(transform.position + transform.up, // Top of capsule
+			transform.position - transform.up, // Bottom of capsule
+			0.5f, // Radius of capsule
+			transform.forward, // Direction of raycast
+			1f); // Range
 		if (Input.GetKey(KeyCode.W) && !capsuleCastHitWall) { // Use GetKey so it refreshes every frame (as oppoosed to GetKeyDown).
 			transform.position += transform.forward * moveSpeed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.S)) {
+		bool capsuleCastHitWallBehind = Physics.CapsuleCast(transform.position + transform.up, // Top of capsule
+			transform.position - transform.up, // Bottom of capsule
+			0.5f, // Radius of capsule
+			-transform.forward, // Direction of raycast
+			1f); // Range
+		if (Input.GetKey(KeyCode.S) && !capsuleCastHitWallBehind) {
 			transform.position += -transform.forward * moveSpeed * Time.deltaTime;
 		}
 		#endregion
