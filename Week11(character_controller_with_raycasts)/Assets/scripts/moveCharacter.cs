d69bb8b2fd6270d922aef6ffd33eaf73fad39f6e
J. Matthew Griffis <grifj153@newschool.edu>
2013-04-22 08:48:35 -0400

    Revamped the level layout. Capsulecast is sort of working.

--- Week11(character_controller_with_raycasts)/Assets/scripts/moveCharacter.cs
+++ Week11(character_controller_with_raycasts)/Assets/scripts/moveCharacter.cs
@@ -7,24 +7,24 @@ public class moveCharacter : MonoBehaviour {
 	float capsuleRad;
 	public float moveSpeed;
 	public float turnRate;
-	float yPos;
 	float yVel;
 	public float gravity;
 	public float maxJump;
 	Vector3 origin;
 	float abyss;
+	public float triggerGrav;
 
 	// Use this for initialization
 	void Start () {
 		capsuleRad = 0.5f;
-		moveSpeed = 3.0f;
+		moveSpeed = 5.0f;
 		turnRate = 125.0f;
-		yPos = 0;
 		yVel = 0;
 		gravity = -0.5f;
 		maxJump = 15f;
-		origin = new Vector3(0,10,0);
+		origin = new Vector3(0,20,0);
 		abyss = -10;
+		triggerGrav = 2;
 	}
 	
 	// Update is called once per frame
@@ -33,25 +33,21 @@ public class moveCharacter : MonoBehaviour {
 		if (transform.position.y < abyss) transform.position = origin; // If you fall off the edge, warp back to the beginning.
 		
 		#region GRAVITY
-        if (!Physics.Raycast(transform.position, -transform.up, 1f) && yVel == 0) { // Shoot a raycast downward; if it doesn't hit anything within 1 distance and we're not jumping...
+        if (!Physics.Raycast(transform.position, -transform.up, triggerGrav) && yVel == 0) { // Shoot a raycast downward; if it doesn't hit anything within 1 distance and we're not jumping...
 			transform.position += Physics.gravity * Time.deltaTime; // ...move downward with gravity.
     	}
 		#endregion
 		
 		#region MOVING // This lets you collapse certain code sections easily.
-		bool capsuleCastHitWall = Physics.CapsuleCast(transform.position + transform.up, // Top of capsule
-			transform.position - transform.up, // Bottom of capsule
-			capsuleRad, // Radius of capsule
-			transform.forward, // Direction of raycast
-			1f); // Range
+		
+		Vector3 p1 = transform.position - transform.up; // Top of capsule
+		Vector3 p2 = transform.position + transform.up; // Bottom of capsule
+		
+		bool capsuleCastHitWall = Physics.CapsuleCast(p1, p2, capsuleRad, transform.forward, capsuleRad);
 		if (Input.GetKey(KeyCode.W) && !capsuleCastHitWall) { // Use GetKey so it refreshes every frame (as oppoosed to GetKeyDown).
 			transform.position += transform.forward * moveSpeed * Time.deltaTime; // Move forward if there's no wall.
 		}
-		bool capsuleCastHitWallBehind = Physics.CapsuleCast(transform.position + transform.up, // Top of capsule
-			transform.position - transform.up, // Bottom of capsule
-			capsuleRad, // Radius of capsule
-			-transform.forward, // Direction of raycast
-			1f); // Range
+		bool capsuleCastHitWallBehind = Physics.CapsuleCast(p1, p2, capsuleRad, -transform.forward, capsuleRad);
 		if (Input.GetKey(KeyCode.S) && !capsuleCastHitWallBehind) {
 			transform.position += -transform.forward * moveSpeed * Time.deltaTime; // Move backward if there's no wall.
 		}
@@ -63,13 +59,13 @@ public class moveCharacter : MonoBehaviour {
 		
 		transform.position += new Vector3(0, yVel, 0) * Time.deltaTime; // Jump if on a surface and there's nothing overhead.
 		
-		if (Input.GetKey(KeyCode.J) && allowJump == true && Physics.Raycast(transform.position, -transform.up, 1f)) {
+		if (Input.GetKey(KeyCode.J) && allowJump == true && Physics.Raycast(transform.position, -transform.up, triggerGrav)) {
 			yVel = maxJump;
 			allowJump = false;
 		}
 		else if (!Input.GetKey(KeyCode.J)) {
 			yVel = 0;
-			if (Physics.Raycast(transform.position, -transform.up, 1f)) allowJump = true;
+			if (Physics.Raycast(transform.position, -transform.up, triggerGrav)) allowJump = true;
 		}
 		if (Physics.Raycast(transform.position, transform.up, 1.0f)) yVel = 0;
 		#endregion
@@ -83,5 +79,8 @@ public class moveCharacter : MonoBehaviour {
 			transform.Rotate(Vector3.up * turnRate * Time.deltaTime);
 		}
 		#endregion
+		
+		//Debug.DrawLine;
+		//Debug.DrawRay;
 	}
 }
